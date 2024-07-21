5bfcb1fa519308ed8b7a7b02318f15601849b80a
abhishek nair <abhisheknair@gmail.com>
2024-07-21 15:58:19 +0530

    login functionality and blog feature added

--- server.js
+++ server.js
@@ -1,13 +1,64 @@
-const express = require("express")
 const articleRouter = require("./routes/articles")
+const express = require("express")
+const path = require("path")
 const app = express()
+const bcrypt = require("bcrypt")
 
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 
 app.use("/articles", articleRouter)
 
+app.use(express.urlencoded({ extended: false}))
+
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    const articles = [{
+        title: "Welcome to my blog",
+        createdAt: new Date(),
+        description: "This is my first blog post"
+    },
+    {
+        title: "Welcome to my blog2",
+        createdAt: new Date(),
+        description: "This is my second blog post"
+    }]
+    res.render("index", { articles: articles})
 })
 
-app.listen(5000)
+
+
+
+
+
+
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs")
+})
+
+app.get("/register", (req, res) => {
+    res.render("register.ejs")
+})
+
+app.post("/login", async (req, res) => {
+    res.redirect("/")
+})
+
+app.post("/register", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect("/login")
+    } catch  {
+        res.redirect("/register")
+    }
+    console.log(users)
+})
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000")
+})

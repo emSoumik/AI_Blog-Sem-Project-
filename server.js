f335de416458394ebf0c8298b60dee826739cd5f
emSoumik <soumik.halder026@gmail.com>
2024-12-20 10:50:01 +0530

    Add Next.js configuration and enhance server setup for dynamic port handling

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const app = express()
 mongoose.connect('mongodb+srv://abhisheknair616:faceless123@cluster0.8eyay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 app.set('view engine', 'ejs')
+app.use(express.static("public"))
+
 
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
@@ -150,6 +152,19 @@ app.use((req, res, next) => {
   res.status(404).send('Not Found')
 })
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000")
-})
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+server.on('error', (err) => {
+  if (err.code === 'EADDRINUSE') {
+    console.error(`Port ${port} is already in use. Trying another port...`);
+    const newPort = port + 1;
+    app.listen(newPort, () => {
+      console.log(`Server running on port ${newPort}`);
+    });
+  } else {
+    throw err;
+  }
+});

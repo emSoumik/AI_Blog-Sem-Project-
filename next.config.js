165965fff922ba5496285719bcbac6ae6158d886
emSoumik <soumik.halder026@gmail.com>
2024-12-20 10:52:04 +0530

    Add experimental outputFileTracingRoot configuration and update Next.js version in package.json

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: __dirname,
+  },
 }
 
 module.exports = nextConfig
